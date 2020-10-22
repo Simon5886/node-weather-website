1800cc51b7462e45065a4c8b44e8c0cb9e9e8758
Simon Marshall <simon5886@mac.com>
2020-10-22 10:54:16 +0100

    preparing for deployment

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ console.log(path.join(__dirname, '../public'))
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 //setup handlebars and views location.
 app.set('view engine', 'hbs')
